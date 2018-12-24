79484cebd83b5f4d7b0219a4a90bc942ab183e1b
waditya <waditya02@gmail.com>
2018-12-24 14:22:52 +0530

    Added new collections and documents

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,34 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+  if(err){
+    return console.log('Unable to connect to MongoDB Server');
+  }
+  console.log('Connected to MongoDB Server');
+
+  const db = client.db('TodoApp');
+
+  // db.collection('Todos').insertOne({
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) => {
+  //   if(err){
+  //     return console.log('unable to insert todo item', err);
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  // });
+
+  db.collection('Users').insertOne({
+    name: 'Aditya',
+    age: 29,
+    location: 'Pune'
+  }, (err, result) => {
+    if(err){
+      return console.log('unable to insert user item', err);
+    }  
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+  client.close(); //Closes connection with MongoDB Server
+  console.log('MongoDB connection closed');
+});
