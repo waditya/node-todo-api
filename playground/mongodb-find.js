c3142768f9421cfd22f131a02f24dc14aa2e73b9
waditya <waditya02@gmail.com>
2018-12-24 15:25:35 +0530

    Added New filters based on object id

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -7,12 +7,24 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   console.log('Connected to MongoDB Server');
 
   const db = client.db('TodoApp');
-  db.collection('Todos').find().toArray().then((docs)=> {
+  db.collection('Todos').find({completed:false}).toArray().then((docs)=> {
     console.log(docs);
     console.log(JSON.stringify(docs, undefined, 2));
   }, (err)=> {
       console.log('Unable to fetch todos', err);
   });
+
+  console.log('Filter based on Object ID');
+  
+  db.collection('Todos').find({
+    _id : new ObjectID('5c20a4618cb5c9dbceecfff1')}).toArray().then((docs)=> {
+    console.log(docs);
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err)=> {
+      console.log('Unable to fetch todos', err);
+  });
+
+
   client.close(); //Closes connection with MongoDB Server
   console.log('MongoDB connection closed');
 });
