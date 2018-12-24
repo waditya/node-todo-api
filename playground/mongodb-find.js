3ef733cabe4bb549bdb885f3255b40ef7f0a1ee5
waditya <waditya02@gmail.com>
2018-12-24 15:44:07 +0530

    Added count functionality

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -8,22 +8,27 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
 
   const db = client.db('TodoApp');
   db.collection('Todos').find({completed:false}).toArray().then((docs)=> {
-    console.log(docs);
+    // console.log(docs);
     console.log(JSON.stringify(docs, undefined, 2));
   }, (err)=> {
       console.log('Unable to fetch todos', err);
   });
 
   console.log('Filter based on Object ID');
-  
+
   db.collection('Todos').find({
     _id : new ObjectID('5c20a4618cb5c9dbceecfff1')}).toArray().then((docs)=> {
-    console.log(docs);
     console.log(JSON.stringify(docs, undefined, 2));
   }, (err)=> {
       console.log('Unable to fetch todos', err);
   });
 
+  db.collection('Todos').find().count().then((count)=> {
+    console.log('Number of records', count);
+
+  }, (err)=> {
+      console.log('Unable to number of records', err);
+  });
 
   client.close(); //Closes connection with MongoDB Server
   console.log('MongoDB connection closed');
