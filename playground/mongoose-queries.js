89cedf3f84e088c97e2c2477945f3c30dd83e5af
waditya <waditya02@gmail.com>
2018-12-29 00:18:30 +0530

    Added Todo ID Validation, findOne

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,16 +1,31 @@
+const {ObjectID} = require('mongodb');
+
 const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 
 var id = '5c2667cd69386468dc415dd5';
 
-Todo.find({
-  _id: id //Mongoose converst String into ObjectID
-}).then((todos)=> {
-  console.log('Todos :', todos);
-});
+if (!ObjectID.isValid(id)){
+  console.log('Id not valid!');
+}
 
-Todo.findOne({
-  _id: id //Mongoose converst String into ObjectID
-}).then((todo)=> {
+// Todo.find({
+//   _id: id //Mongoose converst String into ObjectID
+// }).then((todos)=> {
+//   console.log('Todos :', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id //Mongoose converst String into ObjectID
+// }).then((todo)=> {
+//   console.log('Todo :', todo);
+// });
+
+Todo.findById(id).then((todo)=> {
+  if(!todo){
+    return console.log('Id not found!');
+  }
   console.log('Todo :', todo);
+}).catch((error)=> {
+  console.log(`Error is ${error}`);
 });
