cd907c48d7d9c7d9023d90fb6430943a5bf80763
waditya <waditya02@gmail.com>
2018-12-28 15:58:29 +0530

    Refactor Mongoose

--- server/server.js
+++ server/server.js
@@ -7,3 +7,28 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
+var app = express(); //Stores express application
+
+// // COMBAK: Configure Middleware
+
+app.use(bodyParser.json()) // Function returned by bodyParser.json() is the middleware
+
+app.post('/todos', (req, res)=> {
+  var todo = new Todo({
+    text: req.body.text
+  });
+
+  todo.save().then((doc)=> {
+    res.send(doc);
+  }, (error)=> {
+    res.send(error);
+  })
+  console.log(req.body);
+});
+
+// GET /todos/123r3458984
+
+app.listen(3000, ()=> {
+  console.log('Started on port 3000');
+}); //Basic Server Created
