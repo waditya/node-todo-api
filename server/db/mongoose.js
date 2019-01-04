183800d04fd7b6bcb425bca56c973906f5606106
waditya <waditya02@gmail.com>
2019-01-04 14:19:40 +0530

    Updated Heroku Configuration

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://adm_aditya:Jan@2019@ds149404.mlab.com:49404/todo-app-api'
+  mlab: 'mongodb://adm_aditya:Jan\@2019@ds149404.mlab.com:49404/todo-app-api'
 }
 // mongoose.connect();
 // mongoose.connect(db.localhost || db.mlab);
