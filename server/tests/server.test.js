3fb21415a47c7231afd982b6e34e1de178115988
waditya <waditya02@gmail.com>
2019-01-04 09:21:46 +0530

    Added New Testcases to verify 404 messages

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -85,8 +85,28 @@ describe('GET /todos/:id', () => {
       .get(`/todos/${todos[0]._id.toHexString()}`)
       .expect(200)
       .expect((res)=> {
-        expect(res.body.todos.text).toBe(todos[0].text);
+        expect(res.body.todo.text).toBe(todos[0].text);
       })
-      .end(done)
-  })
+      .end(done);
+  });
+
+    it('should return 404 if todo not found', (done)=>{
+      // make sure you get a 404 back
+      var hexId = new ObjectID().toHexString();
+
+      request(app)
+        .get(`/todos/${hexId}`)
+        .expect(404) //Assertions
+        .end(done);
+      });
+
+
+    it('should return 404 for non-object ids', (done)=>{
+      // /todos/123
+      request(app)
+        .get(`/todos/123`)
+        .expect(404) //Assertions
+        .end(done);
+    });
+
 });
