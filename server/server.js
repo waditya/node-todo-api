861c8a6646ad8fba8b297e580eaee0c5c5f82790
waditya <waditya02@gmail.com>
2019-01-15 03:32:44 -0600

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -88,6 +88,33 @@ app.delete('/todos/:id', (req, res)=>{
   });
 });
 
+app.patch('/todos/:id', (req, res)=>{
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo)=>{
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo});
+    
+  }).catch((e)=>{
+    res.status(400).send();
+  })
+
+});
+
 app.listen(port, ()=> {
   console.log(`Started on port ${port}`);
 }); //Basic Server Created
